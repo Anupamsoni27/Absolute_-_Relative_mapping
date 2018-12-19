04e0e90d95734771a4b1f5bce54bf7f312729116
Anupam Soni <anupamsoni27@gmail.com>
2018-12-19 23:48:52 +0530

    updated

--- s.py
+++ s.py
@@ -8,7 +8,7 @@ import datetime
 # current_datetime = "29-12-2015"
 # FMT = '%d-%m-%Y'
 # current_datetime = datetime.datetime.strptime(current_datetime, FMT)
-# from sympy import primenu
+#from sympy import primenu
 
 
 # model_list = list(model_list)
@@ -16,14 +16,14 @@ import datetime
 # print(model_list)
 global obeservation_list
 obeservation_list = []
-path = "C://Users//anupam.soni//PycharmProjects//Absolute_&_Relative_mapping//"
+path ="C://python_tool//Absolute_Relative_mapping//"
 class main():
     def __init__(self, name):
         self.name = name
 
     def method2(self, temp_date, temp_model, temp_relative_model_code):
-        print("Runing method_2...")
-        df2 = pd.read_excel(path+"file.xlsx",
+        df2 = pd.read_excel(
+            "C://python_tool//Absolute_Relative_mapping//input//file.xlsx",
             sheet_name='data', index_col=None, header=[0])
         rel_model_list = df2.columns
         for t_model in rel_model_list:
@@ -32,30 +32,26 @@ class main():
                 df3 = df2[[df2.columns[0], temp_model, t_model]]
                 df3.columns.values[0] = "Date"
                 df3 = df3[df3["Date"] == temp_date]
-                a = df3  
+
                 df3["observation_list"]= list(df3[temp_model] == df3[t_model])
 
                 observation_list = list(df3["observation_list"])
                 for a in observation_list:
                     if "False" ==str(a):
-                     df3.to_csv(path+"fffff.csv")
-                     # print(float(df3.ix[1,temp_model]))
-                     absolute_value = float(df3.iloc[0,1])
-                     relative_value = float(df3.iloc[0,2])
-                     # print( df3)
-                     temp_row = temp_date.strftime('%d-%m-%Y'), temp_model, t_model, absolute_value,relative_value,bool(absolute_value== relative_value)
-                     print(temp_row)
-                     obeservation_list.append(temp_row)
+                        #absolute_value = float(df3.ix[(1),(temp_model)])
+                        #relative_value = float(df3.ix[(1), (t_model)])
+                        absolute_value = float(df3.iloc[0,1])
+                        relative_value = float(df3.iloc[0,2])
+                        temp_row = temp_date.strftime('%d-%m-%Y'), temp_model, t_model, absolute_value,relative_value,bool(absolute_value== relative_value)
+                        print(temp_row)
+                        obeservation_list.append(temp_row)
     #             df3.to_csv("file"+str(temp_relative_model_code)+".csv")
     # df1 = df[df["Date"]== temp_date]
 
     def method_1(self, model, df):
-        
+
         if "Date" not in model and len(str(model.split("/")[-3]).split(".")[-1]) > 4:
-            print(model)
-            print("Runing method_1...")
-            if abs(int(df.loc[:, model].sum()))> 0:
-                
+            if int(df.loc[:, model].sum()) > 0:
                 small_df = df[[df.columns[0], model]]
                 # print(small_df.head())
                 for index, row in small_df.iterrows():
@@ -110,19 +106,19 @@ class main():
                             # print(model, index)
                             # print(int(contract_period), int(temp_date_month))
                             # print(temp_relative_model_code)
-                            print(temp_date, temp_model, temp_relative_model_code)
+
                         dbObj.method2(temp_date, temp_model, temp_relative_model_code)
 
 
 dbObj = main("Connect MS SQL")
 global df
-xl = pd.ExcelFile(path+"file.xlsx")
+xl = pd.ExcelFile("C://python_tool//Absolute_Relative_mapping//input//file.xlsx")
 
 print(xl.sheet_names)
-df_rollover = pd.read_excel(path+"file.xlsx",
+df_rollover = pd.read_excel("C://python_tool//Absolute_Relative_mapping//input//file.xlsx",
                    sheet_name=xl.sheet_names[1], index_col=None, header=[0])
 
-df = pd.read_excel(path+"file.xlsx",
+df = pd.read_excel("C://python_tool//Absolute_Relative_mapping//input//file.xlsx",
                    sheet_name=xl.sheet_names[0], index_col=None, header=[0])
 df.columns.values[0] = "Date"
 global model_list
@@ -131,8 +127,8 @@ for model in model_list:
     if "Date" not in model and len(str(model.split("/")[-3]).split(".")[-1]) > 4:
         dbObj.method_1(model, df)
 obeservation_df = pd.DataFrame(obeservation_list, columns=["Date Index", "Absolute Model Code", "Relative model Code", "Absolute Value", "Relative Value", "Observation"])
-obeservation_df.to_csv(path +"obeservation_file.csv",index=False)
-writer = ExcelWriter(path+"output.xlsx")
+#obeservation_df.to_csv("obeservation_file.csv",index=False)
+writer = ExcelWriter(path+'output/output.xlsx')
 df.to_excel(writer, 'Data Checked', index=False)
 df_rollover.to_excel(writer, 'Sheet2', index=False)
 obeservation_df.to_excel(writer, 'Validation Report', index=False)
